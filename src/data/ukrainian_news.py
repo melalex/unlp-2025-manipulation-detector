12cea4f5227961523927760623d9eccfc01fe004
melalex <alexander.melashchenko@gmail.com>
2025-03-18 19:13:09 +0000

    Fine tuned xml-roberta

--- src/data/ukrainian_news.py
+++ src/data/ukrainian_news.py
@@ -1,20 +1,27 @@
 from pathlib import Path
 from bs4 import BeautifulSoup
-from datasets import load_dataset, DatasetDict
+from datasets import load_dataset, Dataset
 import multiprocessing
 
 import torch
 
 
-def load_ukrainian_news_dataset(cache_folder, original_cache_folder, tokenizer):
+def load_ukrainian_news_dataset(cache_folder, original_cache_folder, tokenizer, rows_count, max_tokens=512):
     def remove_html_tags(example):
         soup = BeautifulSoup(example["text"], "html.parser")
         return {"text": soup.get_text()}
 
+    def preprocess_function(examples):
+        return tokenizer(
+            examples["text"],
+            truncation=True,
+            max_length=max_tokens,
+        )
+
     cache_path = cache_folder / "ukrainian-news"
 
     if cache_path.exists():
-        return DatasetDict.load_from_disk(cache_path)
+        return Dataset.load_from_disk(cache_path)
 
     core_count = multiprocessing.cpu_count()
 
@@ -23,12 +30,13 @@ def load_ukrainian_news_dataset(cache_folder, original_cache_folder, tokenizer):
 
     dataset = load_dataset(
         "zeusfsx/ukrainian-news",
-        split="train[:10000]",
+        split=f"train[:{rows_count}]",
         cache_dir=original_cache_folder,
         num_proc=core_count,
     )
     dataset = dataset.select_columns(["text"])
     dataset = dataset.map(remove_html_tags, num_proc=core_count)
+    dataset = dataset.map(preprocess_function, remove_columns=["text"], num_proc=core_count, batched=True)
 
     dataset.save_to_disk(cache_path)
 
